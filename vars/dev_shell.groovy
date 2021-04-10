4a8144b203ec3623dc00bdd9b95f63a1d30565dc
Qais Yousef <qyousef@layalina.io>
2021-04-10 20:21:05 +0000

    Add all requied dev commands
    With a testing easter egg hidden inside.
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- vars/dev_shell.groovy
+++ vars/dev_shell.groovy
@@ -1,9 +1,10 @@
-def call(src, dst) {
+def call(cmd) {
 	switch (env.MYCI_NODE_TYPE) {
 	case "android":
+		error "Not implemented for this node type: " + env.MYCI_NODE_TYPE
 		if (env.IPADDRESS && env.PORT) {
 			sh """
-				adb -s ${IPADDRESS}:${PORT} push ${src} ${dst}
+				adb -s ${IPADDRESS}:${PORT} shell ${cmd}
 			"""
 		} else {
 			error "Missing IPADDRESS and/or PORT info"
