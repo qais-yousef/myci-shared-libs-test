766c0f39bd7b6c27c7f43f1c748c4c07cd9fb2cf
Qais Yousef <qyousef@layalina.io>
2021-04-10 20:15:45 +0000

    dev_push: change quoting to see if this helps making it work
    for some reason ${src} and ${dst} end up being empty..
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- vars/dev_push.groovy
+++ vars/dev_push.groovy
@@ -2,9 +2,9 @@ def call(src, dst) {
 	switch (env.MYCI_NODE_TYPE) {
 	case "android":
 		if (env.IPADDRESS && env.PORT) {
-			sh '''
+			sh """
 				adb -s ${IPADDRESS}:${PORT} push ${src} ${dst}
-			'''
+			"""
 		} else {
 			error "Missing IPADDRESS and/or PORT info"
 		}
