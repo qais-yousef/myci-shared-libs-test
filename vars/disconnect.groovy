bddab5556cc0d991df040eafbd14bc1f1a4ca9f6
Qais Yousef <qyousef@layalina.io>
2021-04-10 17:34:48 +0000

    disconnect.groovy: disconnect based on node type
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- vars/disconnect.groovy
+++ vars/disconnect.groovy
@@ -1,8 +1,17 @@
 def call() {
-	if (env.IPADDRESS && env.PORT) {
-		sh '''
-			adb -s ${IPADDRESS}:${PORT} shell "echo temp > /sys/power/wake_unlock"
-			adb disconnect ${IPADDRESS}:${PORT}
-		'''
+	switch (env.MYCI_NODE_TYPE) {
+	case "android":
+		if (env.IPADDRESS && env.PORT) {
+			sh '''
+				adb -s ${IPADDRESS}:${PORT} shell "echo temp > /sys/power/wake_unlock"
+				adb disconnect ${IPADDRESS}:${PORT}
+			'''
+		} else {
+			println("ERROR: Missing IPADDRESS and PORT info")
+			exit(1)
+		}
+		break
+	default:
+		break
 	}
 }
