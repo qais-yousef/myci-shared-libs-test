e57b52b4a56f9e3b563162d93e20ba33c38adf2a
Qais Yousef <qyousef@layalina.io>
2021-04-10 16:56:58 +0000

    Try to fix inability to access en variables
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- vars/target.groovy
+++ vars/target.groovy
@@ -1,13 +1,13 @@
 def connect_android() {
 	sh '''
-		adb shell connect ${env.IPADDRESS}:${env.PORT}
-		adb shell -s ${env.IPADDRESS}:${env.PORT} "echo temp > /sys/power/wake_lock"
+		adb shell connect env.IPADDRESS:env.PORT
+		adb shell -s env.IPADDRESS:env.PORT "echo temp > /sys/power/wake_lock"
 	'''
 }
 
 def disconnect_android() {
 	sh '''
-		adb shell -s ${env.IPADDRESS}:${env.PORT} "echo temp > /sys/power/wake_unlock"
-		adb shell disconnect ${env.IPADDRESS}:${env.PORT}
+		adb shell -s env.IPADDRESS:env.PORT "echo temp > /sys/power/wake_unlock"
+		adb shell disconnect env.IPADDRESS:env.PORT
 	'''
 }
