369bb32e7e91527b6df06466530bde88fe34ff87
Qais Yousef <qyousef@layalina.io>
2021-04-10 17:32:17 +0000

    connect.groovy: auto connect based on node type
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- vars/connect.groovy
+++ vars/connect.groovy
@@ -1,10 +1,17 @@
 def call() {
-	println(env.IPADDRESS)
-	println(env.PORT)
-	if (env.IPADDRESS && env.PORT) {
-		sh '''
-			adb connect ${IPADDRESS}:${PORT}
-			adb -s ${IPADDRESS}:${PORT} shell "echo temp > /sys/power/wake_lock"
-		'''
+	switch (env.MYCI_NODE_TYPE) {
+	case "android":
+		if (env.IPADDRESS && env.PORT) {
+			sh '''
+				adb connect ${IPADDRESS}:${PORT}
+				adb -s ${IPADDRESS}:${PORT} shell "echo temp > /sys/power/wake_lock"
+			'''
+		} else {
+			println("ERROR: Missing IPADDRESS and PORT info")
+			exit(1)
+		}
+		break
+	default:
+		break
 	}
 }
